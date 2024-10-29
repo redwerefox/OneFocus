43cc13554e41d4ac862bb40520940872638497a2
_Vulpes <vulpesk1tzune@gmail.com>
2024-10-29 15:54:01 +0100

    Implement Plugin prototype in main.ts

--- main.ts
+++ main.ts
@@ -1,104 +1,51 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { ActivitiesVault, Activity } from 'src/ActivitiesVault';
+import { ActivitiesObserverInterface } from 'src/ActivitiesObserver';
+import { on } from 'events';
 
-// Remember to rename these classes and interfaces!
+// Remember to rename these classes and interfaces! TODO
 
-interface MyPluginSettings {
+interface OneFocusSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const ONEFOCUS_SETTINGS: OneFocusSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+class OneFocusActivityLogUpdater implements ActivitiesObserverInterface {
+	update(activities: Activity[]): void {
+		console.log(activities);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class ActivitiesModal extends Modal {
+	
+	activities: Activity[];
+
+
+	constructor(app: App, activities: Activity[]) {
 		super(app);
+		this.activities = activities;
+	}
+
+	makeActivityList(): HTMLElement {
+		const listEl = document.createElement('ul');
+		listEl.addClass('activity-list');
+
+		this.activities.forEach(activity => {
+			const itemEl = document.createElement('li');
+			itemEl.addClass('activity-item');
+			itemEl.setText(activity.displayName);
+			listEl.appendChild(itemEl);
+		});
+
+		return listEl;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.appendChild(this.makeActivityList());
 	}
 
 	onClose() {
@@ -107,10 +54,26 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class CurrentActivityId {
+	activityId: string;
+	
+	constructor(activityId: string) {
+		this.activityId = activityId;
+	}
 
-	constructor(app: App, plugin: MyPlugin) {
+	getActivity(): string {
+		return this.activityId;
+	}
+
+	setActivity(activityId: string): void {
+		this.activityId = activityId;
+	}
+}
+
+class OneFocusSettingTab extends PluginSettingTab {
+	plugin: OneFocus;
+
+	constructor(app: App, plugin: OneFocus) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -130,5 +93,102 @@ class SampleSettingTab extends PluginSettingTab {
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
+	}
+}
+
+export default class OneFocus extends Plugin {
+	settings: OneFocusSettings;
+
+	activitiesVault: ActivitiesVault;
+	oneFocusActivityLogUpdater: OneFocusActivityLogUpdater;
+
+	activityModal: ActivitiesModal;
+
+	currentActivity: Activity
+
+	index: number;
+
+	
+	async onload() {
+		await this.loadSettings();
+
+		this.index = 1;
+		this.activitiesVault = new ActivitiesVault();
+		this.currentActivity = this.activitiesVault.defaultActivity;
+		this.oneFocusActivityLogUpdater = new OneFocusActivityLogUpdater();
+		this.activitiesVault.subscribe(this.oneFocusActivityLogUpdater);
+
+		//Commands
+
+		this.addCommand({
+			id: 'add-activity',
+			name: 'OneFocus: Add Activity',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const activity = new Activity('New Activity'+ this.index);
+				this.index += 1;
+				this.activitiesVault.addActivity(activity);
+				this.activitiesVault.notifyObservers();
+			}
+
+		});
+
+		this.addCommand({
+			id: 'remove-activity',
+			name: 'OneFocus: Remove Activity',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const activity = new Activity('New Activity' + (this.index - 1));
+				this.index -= 1;
+				this.activitiesVault.removeActivity(activity);
+				this.activitiesVault.notifyObservers();
+			}
+		});
+
+		this.addCommand({
+			id: 'see-activities',
+			name: 'OneFocus: See Activities',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				new ActivitiesModal(this.app, this.activitiesVault.activities).open();
+				//this.currentActivity = activity;
+			}
+		});
+
+		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText(this.currentActivity.displayName); 
+
+		//todo: consider a ribbon icon
+
+		// This creates an icon in the left ribbon.
+		/*const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			new Notice('This is a notice!');
+		});*/
+		// Perform additional things with the ribbon
+		//ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new OneFocusSettingTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, ONEFOCUS_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 }
